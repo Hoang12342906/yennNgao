3202e1d0377cd83275dd671e6b8ef8cdbba9b94d
Hoang12342906 <104152244+Hoang12342906@users.noreply.github.com>
2022-11-12 21:21:10 +0700

    nhac

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const yourDate = new Date("2022-07-06T00:00:00"),
-music = ['DaiDienChoTraiTim','Ketthucsudocthancuaemdi','Neuanhkhongphien','CoEmCho','LanHenHoDauTien','khongsaomaemdayroi','lonhuanhyeuem','AnhDanhRoiNguoiYeunay','LacVaoTrongMo','PhoDaLenDen','TinhCatinhta'];
+music = ['DaiDienChoTraiTim','Neuanhkhongphien','CoEmCho','LanHenHoDauTien','khongsaomaemdayroi','lonhuanhyeuem','AnhDanhRoiNguoiYeunay','LacVaoTrongMo','emmuon','TinhCatinhta','emthich','LoiAnhChuaTheNoi','thichthich','Bingo'];
 
 document.addEventListener('DOMContentLoaded', function(){
       var rootTime = document.querySelector("time");
